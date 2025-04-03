9509b904e5dc368ced14db9401c1fb91bf8320fb
RuslanWorkGit <lulka758@gmail.com>
2025-04-03 10:07:47 +0300

    Create empty project

---
+++ FirebaseTestApp/FirebaseTestApp/FirebaseTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestAppApp.swift
+//  FirebaseTestApp
+//
+//  Created by Ruslan Liulka on 03.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
